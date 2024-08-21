83e3cf0a7b1187a558bf0be06b5277d263bd3335
pmanonline <petersonzoconis@gmail.com>
2024-08-21 13:10:16 +0100

    pushing recent updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
-console.log("Vite alias configuration:", path.resolve(__dirname, "./src"));
-
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
