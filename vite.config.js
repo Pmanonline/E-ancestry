1ca39650b335ba3e9fff363bbbe0eb2d0d62c6da
pmanonline <petersonzoconis@gmail.com>
2024-09-17 23:58:42 +0100

    pushing changes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       onwarn(warning, warn) {
+//         if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+//           return;
+//         }
+//         warn(warning);
+//       },
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-import { defineConfig } from "vite";
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
