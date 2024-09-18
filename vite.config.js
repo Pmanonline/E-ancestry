96af59e7c19c6379b4252131ba5899561e78ab2d
pmanonline <petersonzoconis@gmail.com>
2024-09-18 09:57:45 +0100

    pushing changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: "./src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
