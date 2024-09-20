e4802ddd2db4ceea8c44ec4d2eb4817de4220833
pmanonline <petersonzoconis@gmail.com>
2024-09-20 20:43:36 +0100

    pushing updates

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
